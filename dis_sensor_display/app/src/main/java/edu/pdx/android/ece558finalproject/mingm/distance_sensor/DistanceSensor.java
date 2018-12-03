3344b81b7df667eebe8ad12f7831cfa4f1dcc14f
Ming Ma <mming@pdx.edu>
2018-12-02 16:39:15 -0800

    Improve distance sensor and add the control part for LCD display

--- dis_sensor_display/app/src/main/java/edu/pdx/android/ece558finalproject/mingm/distance_sensor/DistanceSensor.java
+++ dis_sensor_display/app/src/main/java/edu/pdx/android/ece558finalproject/mingm/distance_sensor/DistanceSensor.java
@@ -1,17 +1,20 @@
 package edu.pdx.android.ece558finalproject.mingm.distance_sensor;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
-import android.print.PrinterId;
 import android.util.Log;
 
 import com.google.android.things.pio.Gpio;
 import com.google.android.things.pio.PeripheralManager;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
 
-import static android.content.ContentValues.TAG;
 
 /**
  * Skeleton of an Android Things activity.
@@ -34,11 +37,23 @@ import static android.content.ContentValues.TAG;
  */
 public class DistanceSensor extends Activity {
 
-    private static final String ECHO_PIN = "BCM20";
-    private static final String TRIGGER_PIN = "BCM21";
+    private static final String TAG = DistanceSensor.class.getSimpleName();
+
+    //Variable for Distance Sensor
+    private static final String ECHO_PIN = "BCM17";
+    private static final String TRIGGER_PIN = "BCM4";
     private Gpio mEcho;
     private Gpio mTrigger;
-    private double mDistance;
+    private String mDistance;
+
+    //Variable for LCD display
+    private static final String GPIO_LCD_RS = "BCM26";
+    private static final String GPIO_LCD_EN = "BCM19";
+    private static final String GPIO_LCD_D4 = "BCM21";
+    private static final String GPIO_LCD_D5 = "BCM20";
+    private static final String GPIO_LCD_D6 = "BCM16";
+    private static final String GPIO_LCD_D7 = "BCM12";
+    private Lcd1602 lcd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,21 +87,9 @@ public class DistanceSensor extends Activity {
             e.printStackTrace();
         }
 
-        new Thread(){
-            @Override
-            public void run(){
-                try{
-                    while(true){
-                        readDistance();
-                        Thread.sleep(300);
-                    }
-                } catch (IOException e){
-                    e.printStackTrace();
-                } catch (InterruptedException e){
-                    e.printStackTrace();
-                }
-                }
-            }.start();
+        Thread thread = new Thread(calc_distance);
+        thread.start();
+
         }
 
     protected void readDistance() throws IOException, InterruptedException{
@@ -96,16 +99,17 @@ public class DistanceSensor extends Activity {
         //Hold the trigger High for at least 10 us
         mTrigger.setValue(true);
         Thread.sleep(0, 10000); //10 us
+        mTrigger.setValue(false);
         int mReceiving;
         //Reset the trigger pin
-        while(!mEcho.getValue()){
+        while(mEcho.getValue() == false){
              mReceiving = 0; //make this while loop keep working
         }
         long mTimeStart = System.nanoTime();
         Log.i(TAG, "Echo Arrived.");
 
         //Wait for the end of the pulse on the ECHO pin
-        while(mEcho.getValue()){
+        while(mEcho.getValue() == true){
             mReceiving = 1;
         }
         long mTimeEnd = System.nanoTime();
@@ -113,8 +117,61 @@ public class DistanceSensor extends Activity {
 
         long pulseWidth = mTimeEnd - mTimeStart;
         //Assume the speed of sound in air is about 340m/s
-        mDistance = ((pulseWidth / 1000000000.0) * 340.0 / 2.0) * 100.0;
+        Double Distance = ((pulseWidth / 1000000000.0) * 340.0 / 2.0) * 100.0;
+
+        if(Distance <= 50.00){
+            DecimalFormat df = new DecimalFormat("#.00");
+            String formatedDistance = df.format(Distance);
+            mDistance = formatedDistance;
+        }
         Log.i(TAG, "Distance: " + mDistance + "cm");
+        //System.out.println("Calculated Distance: " + mDistance);
+    }
+
+    private Runnable calc_distance = new Runnable() {
+        @Override
+        public void run() {
+            try{
+                lcd = new Lcd1602(GPIO_LCD_RS, GPIO_LCD_EN, GPIO_LCD_D4, GPIO_LCD_D5, GPIO_LCD_D6, GPIO_LCD_D7);
+                lcd.begin(16, 2);
+
+                while(true){
+                    readDistance();
+                    Thread.sleep(1500);
+                    lcd.clear();
+                    lcd.print("Distance:" + mDistance + "cm");
+                    lcd.setCursor(0, 1);
+                    lcd.print(getCurrentTime());
+                }
+            } catch (IOException e){
+                e.printStackTrace();
+            } catch (InterruptedException i){
+                i.printStackTrace();
+            }
+        }
+    };
+
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (lcd != null) {
+            try {
+                lcd.close();
+            } catch (Exception e) {
+                Log.e(TAG, "Error closing", e);
+            } finally {
+                lcd = null;
+            }
+        }
+    }
+
+    public String getCurrentTime(){
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT-8"));
+        Date currentLocalTime = cal.getTime();
+        DateFormat date = new SimpleDateFormat("MM-dd-yyyy HH:mm");
+        date.setTimeZone(TimeZone.getTimeZone("GMT-8"));
+        String localTime = date.format(currentLocalTime);
+        return localTime;
     }
 
 }
